6f11636197a53ef9fb754b3f5240f2dc0ab99d05
Aarthiselvaraj <112369063+Aarthiselvaraj@users.noreply.github.com>
2022-09-20 10:11:32 +0530

    Add files via upload

---
+++ Greatest_of_numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int a,b,c,num;
+ num=a,b,c;
+ cout<<"\n"<<"Enter the three numbers:";
+   cin>>a>>b>>c;
+ if(char str[]="num")
+    cout<<"invalid input";
+ else if(a>=b && a>=c)
+    cout<<"the largest number is:"<<a;
+ else if(b>=a && b>=c)
+    cout<<"the largest number is:"<<b;
+ else if(c>=a&&c>=b)
+    cout<<"the largest number is:"<<c;
+ else if(a==b && a==c)
+    cout<<"enter the differnt numbers";
+ else
+    cout<<"invalid input";
+}
