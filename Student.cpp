728e72f8190e5d9b697c42f170b15a30801dfaac
Aarthiselvaraj <112369063+Aarthiselvaraj@users.noreply.github.com>
2022-09-21 10:20:47 +0530

    Add files via upload

---
+++ Student.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+    string name;
+public:
+    Student(string s)
+    {
+        name = s;
+    }
+    Student()
+    {
+        name = "Unknown";
+    }
+    void print_name()
+    {
+        cout << name << endl;
+    }
+};
+
+int main()
+{
+    Student s1("Jhon");
+    Student s2;
+    s1.print_name();
+    s2.print_name();
+    return 0;
+}
