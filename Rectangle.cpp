b803387b8ff78c6e285ab9caf92c6614bb3bcedc
Aarthiselvaraj <112369063+Aarthiselvaraj@users.noreply.github.com>
2022-09-23 14:42:19 +0530

    Add files via upload

---
+++ Rectangle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include<string>
+using namespace std;
+void movie(string movie,char character)
+{
+	std::string movie;
+	char character;
+	cout<<"Enter the movie name:";
+	cin>>movie;
+	cout<<"Enter the characters in the movie:";
+	cin>>character;
+	
+};
+int main()
+{
+ cout<<"Enter the movie name:";
+ movie(string movie,char charcter);
+ cout<<"Enter the movie name:";
+ movie(string movie,char charcter);
+ cout<<"Enter the movie name:";
+ movie(string movie,char charcter);
+ cout<<"Enter the movie name:";
+ movie(string movie,char charcter);
+ cout<<"Enter the movie name:";
+ movie(string movie,char charcter);
+ return 0;	
+}
